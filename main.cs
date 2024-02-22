7bdff44bf1a211b64af26867500edd7433033002
UrvantsevSergey <159543075+UrvantsevSergey@users.noreply.github.com>
2024-02-22 12:42:13 +0000

    Начал добавлять решение делением

--- main.cs
+++ main.cs
@@ -4,14 +4,21 @@ class Program
 {
   public static void Main (string[] args)
   {
-    Console.Write("Введите число: ");
-    string number = Convert.ToString(Console.ReadLine());
-    int size = number.Length;
-    int n = size;
-    while (n >= size)
-    {
-      Console.Write($"{number[n - size]}, ");
-      size--;
-    }
+//    Console.Write("Введите число: ");
+//    string number = Convert.ToString(Console.ReadLine());
+//    int size = number.Length;
+//    int n = size;
+//    while (n >= size)
+//    {
+//      Console.Write($"{number[n - size]}, ");
+//      size--;
+//    }
+//  }
+//}
+
+  Console.WriteLine("Введите число: ");
+  int number = Convert.ToInt32(Console.ReadLine());
+  int size = number.ToString().Length;
+  Console.WriteLine($"{size} {number} ");
   }
 }
